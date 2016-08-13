53cc56146ab0b048c5abfa785a6c38f9136cf01f
Vishakha Damle <vishakha@Vishakhas-MacBook-Pro.local>
2016-08-13 21:38:08 +0530

    added permission checl

--- app/src/main/java/com/hindsitesapp/moduletest/MainActivity.java
+++ app/src/main/java/com/hindsitesapp/moduletest/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hindsitesapp.moduletest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,20 +44,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void selectPhotos() {
 
-        int maxPhotos = 2;
-        multiImagePicker = new MultiImagePicker.Builder(maxPhotos)
-                .setOnReceiveListener(new MultiImagePickerListener() {
-                    @Override
-                    public void onImagesPicked(List<PickedPhoto> pickedPhotos) {
-                        for(int i=0; i < pickedPhotos.size(); i++) {
-                            PickedPhoto photo = pickedPhotos.get(i);
-                            Log.d("TAG", " In main activity " + photo.getPhotoPath());
+        if(checkPermission() == PackageManager.PERMISSION_GRANTED) {
+            int maxPhotos = 2;
+            multiImagePicker = new MultiImagePicker.Builder(maxPhotos)
+                    .setOnReceiveListener(new MultiImagePickerListener() {
+                        @Override
+                        public void onImagesPicked(List<PickedPhoto> pickedPhotos) {
+                            for(int i=0; i < pickedPhotos.size(); i++) {
+                                PickedPhoto photo = pickedPhotos.get(i);
+                                Log.d("TAG", " In main activity " + photo.getPhotoPath());
 
+                            }
                         }
-                    }
-                })
-                .build();
-        multiImagePicker.startActivity(getApplicationContext());
+                    })
+                    .build();
+            multiImagePicker.startActivity(getApplicationContext());
+
+        } else {
+            requestPermission();
+        }
+
+
     }
 
     @Override
@@ -77,4 +88,68 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private void requestPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    selectPhotos();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+    private int checkPermission() {
+        return ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
+
 }
